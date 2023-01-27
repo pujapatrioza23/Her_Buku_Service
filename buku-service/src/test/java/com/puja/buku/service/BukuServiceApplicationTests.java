de6edf2ad6eea6560dcfdfa8528eae6e5b2532ae
pujapatrioza23 <113494064+pujapatrioza23@users.noreply.github.com>
2023-01-27 16:18:01 +0700

    Add files via upload

---
+++ buku-service/src/test/java/com/puja/buku/service/BukuServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.puja.buku.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BukuServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
